e806d6e2c4989854a09837e4ea08f605039ff4a3
Filipe Benevenuti <filipe.benevenuti@farmaciassaojoao.com.br>
2018-10-15 13:26:45 -0300

    Adicionado thunk; controle dos done undone; search; muti cb

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,9 +8,10 @@ import multi from 'redux-multi'
 
 import App from './main/app'
 import reducers from './main/reducers'
+import thunk from 'redux-thunk'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store =  applyMiddleware(multi, promise)(createStore)(reducers, devTools)
+const store =  applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}> 
         <App />
--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -16,9 +16,32 @@ export const search = () => {
 }
 
 export const add = (description) => {
-    const request = axios.post(URL, { description })
-    return [
-        {type: 'TODO_ADDED', payload: request },
-        search()
-    ]
+    return dispatch => {
+        axios.post(URL, { description })
+        .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data}))
+        .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsDone = todo => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true })
+        .then(resp => dispatch({type: 'TODO_MARKED_AS_DONE', payload: resp.data}))
+        .then(resp => dispatch(search()))
+    }
+}
+
+export const markAsPending = todo => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false })
+        .then(resp => dispatch({type: 'TODO_MARKED_AS_PENDING', payload: resp.data}))
+        .then(resp => dispatch(search()))
+    }
+}
+
+export const remove = todo => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)        
+        .then(resp => dispatch(search()))
+    }
 }
